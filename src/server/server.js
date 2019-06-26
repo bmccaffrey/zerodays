c3e09e4cf3f1e51ccfe246cfa15af8835d50e0af
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-06-25 19:53:41 -0500

    add User Model boilerplate

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PGClient = require('../server/db');
+const User = require('./User');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,8 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to DB
 PGClient.connect();
+const person = new User('bob', 'stuff');
 
 app.get('/all', async (req, res) => {
+	console.log(person);
 	try {
 		const { rows } = await PGClient.query('SELECT * FROM activity;');
 		console.log(rows);
