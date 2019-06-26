a2dd30c1f9cbb7546959a6986b3c66b1441f901d
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-06-25 23:34:02 -0500

    add getUser method

--- src/server/User.js
+++ src/server/User.js
@@ -1,10 +1,12 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const PGClient = require('../server/db');
+
 class User {
 	constructor(username, password) {
 		this.username = username;
 		this.password = password;
-		this.hashedPassword = bcrypt.hash(password, saltRounds);
+		this.hashedPassword = undefined;
 	}
 
 	save() {
@@ -13,12 +15,37 @@ class User {
 		});
 	}
 
+	/** Verify Correct Password
+	 *
+	 * @param {string} password
+	 */
 	verify(password) {
 		let hash = this.hashedPassword;
 		bcrypt
 			.compare(password, hash)
 			.then(function(res) {
 				console.log(res);
+			})
+			.catch(err => console.log(err));
+	}
+
+	/** Verify User Exists
+	 *
+	 * @param {string} email
+	 */
+	static getUser(email) {
+		const text = 'SELECT * FROM users WHERE email = ($1)';
+		const values = [email];
+		PGClient.query(text, values)
+			.then(res => {
+				let results = res.rows[0];
+				if (!results) {
+					console.log('Return an Error Message');
+				} else {
+					console.log(results);
+					const { password } = res.rows[0];
+					this.hashedPassword = password;
+				}
 			})
 			.catch(err => console.log(err));
 	}
