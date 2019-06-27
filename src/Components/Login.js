a5d27fff6270359b5bf7b33f7f1657e30bf512f3
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-06-26 20:50:35 -0500

    add authentication logic to Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -18,7 +18,25 @@ export default class Login extends Component {
 
 	onSubmit(event) {
 		event.preventDefault();
-		alert('Authenticating');
+		fetch('/api/authenticate', {
+			method: 'POST',
+			body: JSON.stringify(this.state),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (res.status === 200) {
+					this.props.history.push('/');
+				} else {
+					const error = new Error(res.error);
+					throw error;
+				}
+			})
+			.catch(err => {
+				console.error(err);
+				alert('Error logging in please try again');
+			});
 	}
 
 	render() {
