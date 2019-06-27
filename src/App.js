480b91103fe754ee6598ec7b347b3332c276f5b0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-06-26 19:42:49 -0500

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Components/home';
 import Login from './Components/Login';
 
-function App() {
-	return (
-		<div className="App">
-			<Login />
+const App = () => (
+	<Router>
+		<div>
+			<h1>No Zero Days</h1>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+			</ul>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/login" exact component={Login} />
+			</Switch>
 		</div>
-	);
-}
+	</Router>
+);
 
 export default App;
