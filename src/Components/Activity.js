f87d1c2c65e9eacfb123cb5a24773962d478c8d5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-04 16:42:40 -0500

    check for zero day

--- src/Components/Activity.js
+++ src/Components/Activity.js
@@ -8,6 +8,20 @@ function getActivites() {
 		.catch(err => console.log(err));
 }
 
+/**
+ * Determine if has been > 1 Day Since Progressing
+ * @param {Date} dateFromProps
+ * @returns {boolean} Diff > 48 Hours
+ */
+function isZeroDay(dateFromProps) {
+	let lastNonZeroDay = new Date(dateFromProps);
+	let today = new Date();
+	const msInDay = 3600000;
+	let diff = today - lastNonZeroDay;
+	let hoursDiff = diff / msInDay;
+	return hoursDiff >= 48;
+}
+
 function save() {
 	alert('Simulate Save');
 }
