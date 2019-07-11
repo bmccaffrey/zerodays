3106e568e49538ff7c2b66ac5217298d7922d3e6
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-11 18:05:05 -0500

    fill out Home component

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
+import parseJsonResponse from './Utilities/JsonParser';
 
 export default class AreaForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			message: 'Loading...'
+			results: [],
+			feedback: ''
 		};
+		this.getActivities = this.getActivities.bind(this);
+		this.displayActivities = this.displayActivities.bind(this);
 	}
 
 	componentDidMount() {
-		fetch('/api/home')
-			.then(res => res.text())
-			.then(res => this.setState({ message: res }));
+		this.getActivities()
+			.then(resp => {
+				this.setState({ results: resp });
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({ feedback: err });
+			});
+	}
+
+	async getActivities() {
+		return fetch('/all').then(parseJsonResponse);
+	}
+
+	displayActivities(activity) {
+		return <div>{activity.name}</div>;
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>Home</h1>
-				<p>{this.state.message}</p>
+				{this.state.feedback && <h1>{this.state.feedback}</h1>}
+				{this.state.results.length > 0 &&
+					this.state.results.map(activity => this.displayActivities(activity))}
 			</div>
 		);
 	}
