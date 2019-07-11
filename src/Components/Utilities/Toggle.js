b704b25618d5e0684bde561fa1186a101b429dee
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-11 18:22:48 -0500

    add Toggle Utility

---
+++ src/Components/Utilities/Toggle.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+export default class Toggle extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			on: false
+		};
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState({ on: !this.state.on });
+	}
+
+	render() {
+		const { children } = this.props;
+		return children({
+			on: this.state.on,
+			toggle: this.toggle
+		});
+	}
+}
