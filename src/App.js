2439c2b8d1c28ca969c5fc514ec3456852f4d91b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-10 20:52:59 -0500

    remove links & add header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home';
-import TokenTester from './Components/TokenTester';
 import Login from './Components/Login';
 import withAuth from './Components/Utilities/withAuth';
-import LogoutButton from './Components/Elements/LogoutButton';
 import ActivitiesController from './Components/ActivitiesController';
+import Header from './Components/Header';
 const App = () => (
 	<Router>
 		<div>
-			<h1>No Zero Days</h1>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/secret">Protected Route</Link>
-				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<LogoutButton />
-				</li>
-			</ul>
-			<ActivitiesController />
+			<Header />
 			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/secret" component={withAuth(TokenTester)} />
+				<Route path="/" exact component={withAuth(ActivitiesController)} />
 				<Route path="/login" component={Login} />
 			</Switch>
 		</div>
