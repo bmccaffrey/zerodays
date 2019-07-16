02a6965bf5065ad343ba7cb667918267301723a5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-15 21:56:44 -0500

    add options icon to activity rows

--- src/Components/home.js
+++ src/Components/home.js
@@ -4,6 +4,7 @@ import Accordion from './Utilities/Accordion';
 import ChevronRight from './Icons/ChevronRight.svg';
 import ChevronDown from './Icons/ChevronDown.svg';
 import Footer from './Footer';
+import Options from './Icons/Options.svg';
 
 const formatDate = date => {
 	const copy = new Date(date);
@@ -39,13 +40,20 @@ export default class AreaForm extends Component {
 	displayActivities(activity) {
 		return (
 			<Accordion key={activity.id}>
-				<div style={{ clear: 'left', margin: '15px 0px' }}>
+				<div
+					style={{
+						clear: 'left',
+						margin: '15px 0px',
+						height: 24
+					}}
+				>
 					<img
 						src={ChevronRight}
 						alt="arrow to expand content"
-						style={{ float: 'left' }}
+						style={{ float: 'left', marginRight: 10 }}
 					/>
-					<div>{activity.name}</div>
+					<span style={{ verticalAlign: 'middle' }}>{activity.name}</span>
+					<img src={Options} alt="options icon" style={{ float: 'right' }} />
 				</div>
 				<div style={{ marginLeft: '15px' }}>Streak: {activity.streak} days</div>
 				<div style={{ marginLeft: '15px' }}>
