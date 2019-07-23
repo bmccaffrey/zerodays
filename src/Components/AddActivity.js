48e37b571a2d34f4a2edea933e18e01264bcd7ea
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2019-07-23 05:25:26 -0500

    pass new activity name to backend

--- src/Components/AddActivity.js
+++ src/Components/AddActivity.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Save from './Icons/Save.svg';
 import Cancel from './Icons/Cancel.svg';
 import styled from 'styled-components';
@@ -7,6 +7,33 @@ import Toggle from './Utilities/Toggle';
 import Modal from './Elements/Modal';
 
 const AddActivity = () => {
+	const [name, setName] = useState('');
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+
+	function post() {
+		return fetch('/api/create', {
+			method: 'POST',
+			body: JSON.stringify({ name }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+	function createActivity() {
+		setIsLoading(true);
+		post()
+			.then(() => {
+				console.log('Created');
+				setIsError(false);
+			})
+			.catch(err => {
+				console.error(err);
+				setIsError(true);
+			})
+			.finally(() => setIsLoading(false));
+	}
+
 	return (
 		<div>
 			<Toggle>
@@ -15,6 +42,8 @@ const AddActivity = () => {
 						<img src={AddCircle} alt="add activity button" onClick={toggle} />
 						<Modal on={on} toggle={toggle}>
 							<div style={{ display: 'flex', justifyContent: 'center' }}>
+								{isError && <h1>An unexpected error occurred.</h1>}
+								{isLoading && <h1>Loading</h1>}
 								<GridContainer>
 									<h2
 										style={{
@@ -28,6 +57,7 @@ const AddActivity = () => {
 									</h2>
 									<input
 										type="text"
+										value={name}
 										name="activityName"
 										autoFocus
 										placeholder="Activity Name"
@@ -36,16 +66,19 @@ const AddActivity = () => {
 											gridColumnStart: 1,
 											gridColumnEnd: 4
 										}}
+										onChange={e => setName(e.target.value)}
 									/>
 									<img
 										src={Cancel}
 										alt="cancels button"
 										style={{ gridColumnStart: 2, gridColumnEnd: 3 }}
+										onClick={toggle}
 									/>
 									<img
 										src={Save}
 										alt="save button"
 										style={{ gridColumnStart: 3, gridColumnEnd: 4 }}
+										onClick={() => createActivity()}
 									/>
 								</GridContainer>
 							</div>
